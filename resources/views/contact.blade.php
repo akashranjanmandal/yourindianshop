29a35c71be0b69e28fa1267c9b5139e45c4dfb70
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2024-02-21 04:40:00 +0530

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -18,10 +18,10 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/bootstrap-icons.css" rel="stylesheet">
 
-        <link rel="stylesheet" href="css/slick.css"/>
+        <link rel="stylesheet" href="/css/slick.css">
 
         <link href="css/tooplate-little-fashion.css" rel="stylesheet">
 
@@ -56,36 +56,38 @@ https://www.tooplate.com/view/2127-little-fashion
                                 </a>
 
                                 <div class="d-lg-none">
-                                    <a href="sign-in.html" ><span style="font-size:15px">Sign In</span></a>
+                                    <a href="/api/login-page" ><span style="font-size:15px">Sign In</span></a>|
+                                    <a href="/api/signup-page" ><span style="font-size:15px">Sign In</span></a>
+                                    
                                     <!-- <a href="sign-in.html" class="bi-person custom-icon me-3"></a> -->
                                 </div>
 
                                 <div class="collapse navbar-collapse" id="navbarNav">
                                     <ul class="navbar-nav mx-auto">
                                         <li class="nav-item">
-                                            <a class="nav-link active" href="index.html">Home</a>
+                                            <a class="nav-link active" href="">Home</a>
                                         </li>
 
                                         <li class="nav-item">
-                                            <a class="nav-link" href="/pricing">Pricing</a>
+                                            <a class="nav-link" href="/api/pricing">Pricing</a>
                                         </li>
 
                                         <li class="nav-item">
-                                            <a class="nav-link" href="/about">About</a>
+                                            <a class="nav-link" href="/api/about">About</a>
                                         </li>
 
                                         <li class="nav-item">
-                                            <a class="nav-link" href="/faq">FAQs</a>
+                                            <a class="nav-link" href="/api/faq">FAQs</a>
                                         </li>
 
                                         <li class="nav-item">
-                                            <a class="nav-link" href="/contact">Contact</a>
+                                            <a class="nav-link" href="/api/contact">Contact</a>
                                         </li>
                                     </ul>
 
                                     <div class="d-none d-lg-block">
-                                            <a href="sign-in.html" ><span style="font-size:15px">Sign In</span></a>
-                                            <!-- <a href="sign-in.html" class="bi-person custom-icon me-3"></a> -->
+                                            <a href="/api/login-page" ><span style="font-size:15px">Sign In</span></a>|
+                                    <a href="/api/signup-page" ><span style="font-size:15px">Sign In</span></a>
                                         </div>
                                 </div>
                             </div>
@@ -230,11 +232,11 @@ https://www.tooplate.com/view/2127-little-fashion
         </footer>
 
         <!-- JAVASCRIPT FILES -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/Headroom.js"></script>
-        <script src="js/jQuery.headroom.js"></script>
-        <script src="js/slick.min.js"></script>
-        <script src="js/custom.js"></script>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.bundle.min.js"></script>
+        <script src="/js/Headroom.js"></script>
+        <script src="/js/jQuery.headroom.js"></script>
+        <script src="/js/slick.min.js"></script>
+        <script src="/js/custom.js"></script>
     </body>
 </html>
