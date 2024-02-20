ca5f4280aba46f274794c8f2fa2db0a01cb8012d
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2024-02-21 03:31:53 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,19 +9,19 @@
 
         <title>Your Indian Shop</title>
 
-        <!-- /css FILES -->
+        <!-- css FILES -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-        <link href="https://fonts.googleapis.com//css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 
-        <link href="/css/bootstrap.min./css" rel="stylesheet">
-        <link href="/css/bootstrap-icons./css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
 
-        <link rel="stylesheet" href="/css/slick./css"/>
+        <link rel="stylesheet" href="css/slick.css"/>
 
-        <link href="/css/tooplate-little-fashion./css" rel="stylesheet">
+        <link href="css/tooplate-little-fashion.css" rel="stylesheet">
 <!--
 
 Tooplate 2127 Little Fashion
