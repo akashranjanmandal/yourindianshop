1f780ecfbed454a5df9462a7f2e7f640eda40988
Akash Ranjan Mandal <106551226+akashranjanmandal@users.noreply.github.com>
2024-02-21 04:48:46 +0530

    Update sign-in.blade.php

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Tooplate's Little Fashion - Sign In Page</title>
+        <title>Your Indian Shop - Sign In Page</title>
 
         <!-- CSS FILES -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -16,12 +16,12 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 
-        <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('/css/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/bootstrap-icons.css" rel="stylesheet">
 
-        <link rel="stylesheet" href="{{ asset('/css/slick.css') }}">
+        <link rel="stylesheet" href="/css/slick.css">
 
-        <link href="{{asset('/css/tooplate-little-fashion.css') }}" rel="stylesheet">
+        <link href="/css/tooplate-little-fashion.css" rel="stylesheet">
 
 <!--
 
